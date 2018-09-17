df079d58a8e8bba866643b477a9480bc1b1cad41
ChangMin Jeon <wisechang1@naver.com>
2018-09-17 12:42:35 +0900

    IMG Download

--- ChangMin/first.js
+++ ChangMin/first.js
@@ -17,11 +17,9 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload() {
-    this.load.setBaseURL('http://labs.phaser.io');
-
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.image('sky', 'space3.png');
+    this.load.image('logo', 'phaser3-logo.png');
+    this.load.image('red', 'red.png');
 }
 
 function create() {
